30230ef45aba7030d1204cdca6e30c4185db42ba
EricMI <98968734+EricMI-Dapper@users.noreply.github.com>
2022-04-10 18:40:28 -0500

    rutas hijas - forchild()

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,12 @@ import { PagesModule } from './pages/pages.module';
 
 // Componentes
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    NopagefoundComponent,
-  ],
-  imports: [BrowserModule, AppRoutingModule, PagesModule],
+  declarations: [AppComponent, NopagefoundComponent],
+  imports: [BrowserModule, AppRoutingModule, PagesModule, AuthModule],
   providers: [],
   bootstrap: [AppComponent],
 })
